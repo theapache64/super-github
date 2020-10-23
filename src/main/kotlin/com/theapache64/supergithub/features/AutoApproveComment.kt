efedb6ab107341fbaf29e41295b5682030225bd6
theapache64 <theapache64@gmail.com>
2020-10-23 21:00:20 +0530

    ◀️ improve algo

--- src/main/kotlin/com/theapache64/supergithub/features/AutoApproveComment.kt
+++ src/main/kotlin/com/theapache64/supergithub/features/AutoApproveComment.kt
@@ -11,35 +11,47 @@ class AutoApproveComment : BaseFeature {
         private val PR_REVIEW_PAGE_URL_REGEX =
             "https:\\/\\/github\\.com\\/.+?\\/.+?\\/pull\\/\\d+?\\/files#submit-review".toRegex()
 
-        private val APPROVE_MESSAGES = arrayOf(
-            "LGTM. Please merge",
-            "Wow. Good job mate. LGTM, Feel free to merge",
-            "Cool. LGTM. Please merge"
+        private val approveActions = mapOf(
+            "approve" to arrayOf(
+                "LGTM. Please merge",
+                "Wow. Good job mate. LGTM, Feel free to merge",
+                "Cool. LGTM. Please merge"
+            )
         )
+
+        private const val RADIO_SELECTOR =
+            "#review-changes-modal > div > div > div > form > div > label > input[type=radio]"
     }
 
     override suspend fun onGitHubPageLoaded() {
         val url = window.location.href
-        println("URL is -> $url")
         val isPrReviewUrl = PR_REVIEW_PAGE_URL_REGEX.matches(url)
-        println("isPrReviewUrl -> $isPrReviewUrl")
+
         if (isPrReviewUrl) {
-            document
-                .querySelector("#review-changes-modal > div > div > div > form > div > label > input[type=radio][value=approve]")
-                ?.let { element ->
-                    element.addEventListener("click", object : EventListener {
-                        override fun handleEvent(event: Event) {
-                            document.querySelector("#pull_request_review_body")?.let { commentTextArea ->
-                                val currentComment = commentTextArea.textContent
-                                println("Current comment is '$currentComment'")
-                                // Either comment should be empty or any of the approve message should be the content
-                                if (currentComment?.isBlank() == true || APPROVE_MESSAGES.contains(currentComment)) {
-                                    commentTextArea.textContent = APPROVE_MESSAGES.random()
+
+            for (action in approveActions.keys) {
+
+                val messages = approveActions[action] ?: error("TSH: Couldn't find key $action")
+
+                document
+                    .querySelector("$RADIO_SELECTOR[value=$action]")
+                    ?.let { element ->
+                        element.addEventListener("click", object : EventListener {
+                            override fun handleEvent(event: Event) {
+                                document.querySelector("#pull_request_review_body")?.let { commentTextArea ->
+                                    val currentComment = commentTextArea.textContent
+                                    println("Current comment is '$currentComment'")
+                                    // Either comment should be empty or any of the approve message should be the content
+                                    if (currentComment?.isBlank() == true || messages.contains(currentComment)) {
+                                        commentTextArea.textContent = messages.random()
+                                    }
                                 }
                             }
-                        }
-                    })
-                }
+                        })
+                    }
+            }
+
+
         }
     }
 }
