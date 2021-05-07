9e586c6764ecfd1ce63b9263654743e53b098faf
theapache64 <theapache64@gmail.com>
2021-05-07 23:40:12 +0530

    🌟 add foldable code to new issue also

--- src/main/kotlin/com/theapache64/supergithub/features/FoldableContent.kt
+++ src/main/kotlin/com/theapache64/supergithub/features/FoldableContent.kt
@@ -10,10 +10,13 @@ import org.w3c.dom.events.EventListener
 class FoldableContent : BaseFeature {
     companion object {
         private val COMMENTABLE_PAGE_URL_REGEX =
-            "https:\\/\\/github\\.com\\/.+?\\/.+?\\/(issues|pull)\\/\\d+.*".toRegex()
+            "https:\\/\\/github\\.com\\/.+?\\/.+?\\/(issues|pull|issues)\\/(new|\\d+.*)".toRegex()
 
-        private const val TARGET_CONTAINER_SELECTOR =
+        private const val TARGET_CONTAINER_EXISTING_ISSUE_SELECTOR =
             "#issuecomment-new > div.border-0.border-md.timeline-comment.timeline-comment--caret > form > fieldset > tab-container > div.comment-form-head.tabnav.d-flex.flex-justify-between.mb-2.p-0.tabnav--responsive.flex-column.border-bottom-0.mb-0.mb-lg-2.flex-items-stretch.border-lg-bottom.color-border-primary.flex-lg-items-center.flex-lg-row > markdown-toolbar > div.flex-nowrap.d-none.d-md-inline-block.mr-3"
+
+        private const val TARGET_CONTAINER_NEW_ISSUE_SELECTOR =
+            "#new_issue > div > div > div.flex-shrink-0.col-12.col-md-9.mb-4.mb-md-0 > div > div.timeline-comment.color-bg-canvas.hx_comment-box--tip > div > tab-container > div.comment-form-head.tabnav.d-flex.flex-justify-between.mb-2.p-0.tabnav--responsive.flex-column.border-bottom-0.mb-0.mb-lg-2.flex-items-stretch.border-lg-bottom.color-border-primary.flex-lg-items-center.flex-lg-row > markdown-toolbar > div.flex-nowrap.d-none.d-md-inline-block.mr-3"
         private val MD_FOLDABLE by lazy {
             """
             <md-foldable tabindex="-1" class="toolbar-item tooltipped tooltipped-sw mx-1" aria-label="Add foldable content" role="button">
@@ -32,7 +35,8 @@ class FoldableContent : BaseFeature {
             </details>
         """.trimIndent()
 
-        private const val TEXTAREA_SELECTOR = "#new_comment_field"
+        private const val TEXTAREA_EXISTING_SELECTOR = "#new_comment_field"
+        private const val TEXTAREA_NEW_SELECTOR = "#issue_body"
 
 
     }
@@ -46,7 +50,10 @@ class FoldableContent : BaseFeature {
 
         if (isCommentablePage) {
             document
-                .querySelector(TARGET_CONTAINER_SELECTOR)
+                .querySelector(TARGET_CONTAINER_EXISTING_ISSUE_SELECTOR)
+                .let { container ->
+                    container ?: document.querySelector(TARGET_CONTAINER_NEW_ISSUE_SELECTOR)
+                }
                 ?.let { container ->
                     container.innerHTML = MD_FOLDABLE + container.innerHTML
 
@@ -57,7 +64,7 @@ class FoldableContent : BaseFeature {
                             object : EventListener {
                                 override fun handleEvent(event: Event) {
                                     // add content
-                                    document.querySelector(TEXTAREA_SELECTOR)
+                                    (document.querySelector(TEXTAREA_EXISTING_SELECTOR) ?: document.querySelector(TEXTAREA_NEW_SELECTOR))
                                         ?.let { textArea ->
                                             textArea as HTMLTextAreaElement
                                             val startPos = textArea.selectionStart
