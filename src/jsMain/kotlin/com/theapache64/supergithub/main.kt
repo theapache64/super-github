db32f04d0dd7a32e7961f5a625e23427d8b53a43
theapache64 <theapache64@gmail.com>
2025-09-24 21:10:46 +0530

    Update version to 1.0.8 across files

--- src/jsMain/kotlin/com/theapache64/supergithub/main.kt
+++ src/jsMain/kotlin/com/theapache64/supergithub/main.kt
@@ -27,7 +27,7 @@ suspend fun main() {
 
 suspend fun runSuperGithub() {
     // [latest version - i promise!]
-    println("SuperGitHub v1.0.7")
+    println("SuperGitHub v1.0.8")
 
     val features = listOf(
         RepoCreatedAt(),
