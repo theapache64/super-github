c65a5320e7ee234826c2297f49577de185887eda
theapache64 <theapache64@gmail.com>
2025-09-24 21:09:40 +0530

    .gitignore refreshed

--- src/jsMain/kotlin/com/theapache64/supergithub/data/remote/repos/GetRepoResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.theapache64.supergithub.data.remote.repos
-
-external interface GetRepoResponse {
-    val created_at: String? // 2019-02-11T04:03:06Z
-}
--- src/jsMain/kotlin/com/theapache64/supergithub/features/BaseFeature.kt
+++
@@ -1,5 +0,0 @@
-package com.theapache64.supergithub.features
-
-interface BaseFeature {
-    suspend fun onGitHubPageLoaded()
-}
